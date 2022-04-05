634df7fc832bec8eb5cb15e3c4c55d9fb19c6883
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-04-05 01:32:57 +0100

    Use a global state to control how many times the fetch action is called.

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   HashRouter as Router,
   Route,
@@ -21,33 +21,37 @@ const toggleActive = (e) => {
   e.target.parentNode.className = 'active';
 };
 
-const Navigator = () => (
-  <Router>
-    <div className="main">
-      <div className="header">
-        <img src="https://user-images.githubusercontent.com/20567503/161595022-dd594e3e-cadc-465c-8cbb-38d0467b2da5.png" alt="Website logo" id="logo" />
-        <h1 className="title">Space Travellers&apos; Hub</h1>
-        <ul className="menu-items">
-          <li className="active">
-            <Link to="/" id="rockets" onClick={toggleActive} className="link">Rockets</Link>
-          </li>
-          <li>
-            <a href="#" onClick={toggleActive} className="link">Missions</a>
-          </li>
-          <li>
-            <div className="vertical-line" />
-          </li>
-          <li>
-            <Link to="/profile" id="profile" onClick={toggleActive} className="link">My Profile</Link>
-          </li>
-        </ul>
+const Navigator = () => {
+  const [rocketsFetched, setRocketsFetched] = useState(false);
+
+  return (
+    <Router>
+      <div className="main">
+        <div className="header">
+          <img src="https://user-images.githubusercontent.com/20567503/161595022-dd594e3e-cadc-465c-8cbb-38d0467b2da5.png" alt="Website logo" id="logo" />
+          <h1 className="title">Space Travellers&apos; Hub</h1>
+          <ul className="menu-items">
+            <li className="active">
+              <Link to="/" id="rockets" onClick={toggleActive} className="link">Rockets</Link>
+            </li>
+            <li>
+              <a href="#" onClick={toggleActive} className="link">Missions</a>
+            </li>
+            <li>
+              <div className="vertical-line" />
+            </li>
+            <li>
+              <Link to="/profile" id="profile" onClick={toggleActive} className="link">My Profile</Link>
+            </li>
+          </ul>
+        </div>
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/" element={<Rockets rocketsFetched={rocketsFetched} setRocketsFetched={setRocketsFetched} />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/" element={<Rockets />} />
-      </Routes>
-    </div>
-  </Router>
-);
+    </Router>
+  );
+}
 
 export default Navigator;
