029b7f0160ed9277e01eef9ba5a148459a6ec66b
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-04-05 14:37:48 +0000

    Create a function to handle the onclick to join

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
 const Mission = (mission) => {
-  const { id, name, description } = mission;
+  const {
+    id,
+    name,
+    description,
+    joinMission,
+    joined,
+  } = mission;
+
+  const handleSubmit = (e) => {
+    if (joined) {
+      e.target.classList.removed('not-joined');
+      e.target.classList.add('joined');
+      joinMission(id);
+    }
+  };
+
   return (
     <tr key={id}>
       <td>{name}</td>
       <td>{description}</td>
       <td><p>NOT A MEMBER</p></td>
-      <td><button className="btn" type="button">Join Mission</button></td>
+      <td><button className="btn" onClick={handleSubmit} type="button">Join Mission</button></td>
     </tr>
   );
 };
