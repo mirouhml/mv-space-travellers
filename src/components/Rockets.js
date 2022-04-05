63e6d21681b08a1506c403836eb2a70c6fac0dcb
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-04-05 01:33:29 +0100

    Call redux action to fetch data from api.

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRockets } from '../redux/rockets/rockets'
 
-const Rockets = () => <div>Rockets</div>;
+const Rockets = (props) => {
+  const [rockets, setRockets] = useState([]);
+  const rocketsInit = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() =>{
+    if (!props.rocketsFetched) {
+      dispatch(getRockets());
+      props.setRocketsFetched(true);
+    }
+  }, [dispatch, props]);
+
+  useEffect(() => {
+    if (props.rocketsFetched) {
+      setRockets(rocketsInit);
+    }
+  }, [rocketsInit, props]);
+
+  return (
+    <div>
+      {
+        console.log(rockets)
+      }
+      Rockets
+    </div>
+  );
+}
 
 export default Rockets;
