053d2d498a385d35b657d054c3e894e733c2bc32
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-04-05 12:26:49 +0000

    Move table header into the Missions component

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -3,24 +3,12 @@ import React from 'react';
 const Mission = (mission) => {
   const { id, name, description } = mission;
   return (
-    <table className="mission">
-      <thead>
-        <tr>
-          <th>Mission</th>
-          <th>Description</th>
-          <th>Status</th>
-          {/* <th>Status</th> */}
-        </tr>
-      </thead>
-      <tbody>
-        <tr key={id}>
-          <td>{name}</td>
-          <td>{description}</td>
-        </tr>
-      </tbody>
-
-    </table>
-
+    <tr key={id}>
+      <td>{name}</td>
+      <td>{description}</td>
+      <td><button type="button">NOT A MEMBER</button></td>
+      <td><button type="button">Join Mission</button></td>
+    </tr>
   );
 };
 
