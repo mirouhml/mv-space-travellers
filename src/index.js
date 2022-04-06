d3d29c87cd785a32adcd40b79624cf30b11c4a2f
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-04-06 15:47:23 +0100

    Change website title and remove unnecessary lines.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
 import store from './redux/configureStore';
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </Provider>,
-  document.getElementById('root'),
 );
