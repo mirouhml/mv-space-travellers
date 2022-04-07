5cfbc2ce6b76f681c582e9410c03380a85415416
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-04-07 16:21:05 +0100

    Add Profile component tests.

---
+++ src/components/__tests__/Profile.test.js
@@ -0,0 +1,129 @@
+import { screen, render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from 'react';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import thunk from 'redux-thunk';
+import rocketsReducer from '../../redux/rockets/rockets';
+import missionsReducer from '../../redux/missions/missions';
+import Profile from '../Profile';
+import App from '../../App';
+
+const mockRocketsData = [
+  {
+    id: "rocket-1",
+    image: "rocket-image-1",
+    name: "Rocket 1",
+    description: "rocket-description-1",
+    reserved: false,
+    wikipedia: "rocket-link-1",
+  },
+  {
+    id: "rocket-2",
+    image: "rocket-image-2",
+    name: "Rocket 2",
+    description: "rocket-description-2",
+    reserved: false,
+    wikipedia: "rocket-link-2",
+  },
+  {
+    id: "rocket-3",
+    image: "rocket-image-3",
+    name: "Rocket 3",
+    description: "rocket-description-3",
+    reserved: false,
+    wikipedia: "rocket-link-3",
+  },
+];
+
+const mockMissionsData = [
+  {
+    id: "mission-1",
+    image: "mission-image-1",
+    name: "mission 1",
+    description: "mission-description-1",
+    joined: false,
+    wikipedia: "mission-link-1",
+  },
+  {
+    id: "mission-2",
+    image: "mission-image-2",
+    name: "mission 2",
+    description: "mission-description-2",
+    joined: false,
+    wikipedia: "mission-link-2",
+  },
+  {
+    id: "mission-3",
+    image: "mission-image-3",
+    name: "mission 3",
+    description: "mission-description-3",
+    joined: false,
+    wikipedia: "mission-link-3",
+  },
+];
+
+const rootReducer = combineReducers({
+  rockets: rocketsReducer,
+  missions: missionsReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk),
+);
+
+describe('Profile component tests:', () => {
+  it('Component renders correctly', () => {
+    const profile = renderer.create(
+      <Profile
+        missions={mockMissionsData}
+        rockets={mockRocketsData}
+      />,
+    ).toJSON();
+    expect(profile).toMatchSnapshot();
+  });
+
+  it('No missions message displayed correctly', () => {
+    render(
+      <Profile
+        missions={mockMissionsData}
+        rockets={mockRocketsData}
+      />
+    );
+    expect(screen.getByText('No Missions to display.')).toBeInTheDocument();
+  });
+
+  it('No rockets message displayed correctly', () => {
+    render(
+      <Profile
+        missions={mockMissionsData}
+        rockets={mockRocketsData}
+      />
+    );
+    expect(screen.getByText('No rockets to display.')).toBeInTheDocument();
+  });
+
+  it('Reserve some rockets and check if they are displayed correctly', () => {
+    const reservedRockets = mockRocketsData.map((rocket) => ({ ...rocket, reserved: true }));
+    const { container } = render(
+      <Profile
+        missions={mockMissionsData}
+        rockets={reservedRockets}
+      />
+    );
+    expect(container.querySelector('.my-rockets').childElementCount).toBe(3);
+  });
+
+  it('Join some missions and check if they are displayed correctly', () => {
+    const reservedMissions = mockMissionsData.map((mission) => ({ ...mission, joined: true }));
+    const { container } = render(
+      <Profile
+        missions={reservedMissions}
+        rockets={mockRocketsData}
+      />
+    );
+    expect(container.querySelector('.my-missions').childElementCount).toBe(3);
+  });
+});
